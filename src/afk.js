f8cd8feb6bca0df0c000196901c08e318414d38e
EdwardXie <edwardxie1@gmail.com>
2021-05-12 23:09:25 -0400

    testing commit

--- src/afk.js
+++ src/afk.js
@@ -1,4 +1,15 @@
 //login and afk
-require('./info');
+const bot = require('./info');
 
-
+bot.once('spawn', async() => {
+    await bot.waitForChunksToLoad()
+    bot.chat(``)
+  
+    // let recipe = recipeLookup('piston', '1.16.5')
+    // console.log(recipe)
+    
+    //5  5  5
+    //4 265 4
+    //4 331 4
+    // await craftItem();
+  })
--- src/info.js
+++ src/info.js
@@ -4,9 +4,9 @@ const mineflayer = require('mineflayer')
 const bot = mineflayer.createBot({
   host: process.env.DB_host,             // minecraft server ip
   username: process.env.DB_username, // minecraft username
-  //password: process.env.DB_password,          // minecraft password, comment out if you want to log into online-mode=false servers
+  password: process.env.DB_password,          // minecraft password, comment out if you want to log into online-mode=false servers
   port: process.env.DB_port,                // only set if you need a port that isn't 25565
-  // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+  version: process.env.DB_version,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
 })
 
